a76b8c0ca2ca87e6709075a81619452ccd39d00c
Sheen An <sheen.an.goh@gmail.com>
2020-09-25 15:53:33 +0800

    fixed bug where description and id is hardcoded

--- src/actions.js
+++ src/actions.js
@@ -3,20 +3,20 @@ import { BUG_ADDED, BUG_REMOVED, BUG_RESOLVED } from './actionTypes';
 export const bugAdded = (description) => ({
   type: BUG_ADDED,
   payload: {
-    description: 'First Bug',
+    description,
   },
 });
 
 export const bugRemoved = (id) => ({
   type: BUG_REMOVED,
   payload: {
-    id: 1,
+    id,
   },
 });
 
 export const bugResolved = (id) => ({
   type: BUG_RESOLVED,
   payload: {
-    id: 1,
+    id,
   },
 });
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import customStore from './customStore';
 import reduxStore from './store';
 import { bugAdded, bugRemoved, bugResolved } from './actions';
 
-const USE_CUSTOM_STORE = true;
+const USE_CUSTOM_STORE = false;
 let store;
 
 if (!USE_CUSTOM_STORE) {
@@ -16,8 +16,12 @@ const unsubscribe = store.subscribe(() => {
 });
 
 store.dispatch(bugAdded('First Bug'));
+store.dispatch(bugAdded('Second Bug'));
+store.dispatch(bugAdded('Bug 3'));
+store.dispatch(bugAdded('Bug 4'));
 
 store.dispatch(bugResolved(1));
+store.dispatch(bugResolved(2));
 unsubscribe();
 store.dispatch(bugRemoved(1));
 
