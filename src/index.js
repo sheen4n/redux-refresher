e15e9cc6d2379d73733fde8135c6ed669f1cb0fc
Sheen An <sheen.an.goh@gmail.com>
2020-09-25 15:40:28 +0800

    Implemented Custom Store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import store from './store';
+import customStore from './customStore';
+import reduxStore from './store';
 import { bugAdded, bugRemoved, bugResolved } from './actions';
+
+const USE_CUSTOM_STORE = true;
+let store;
+
+if (!USE_CUSTOM_STORE) {
+  store = reduxStore;
+} else {
+  store = customStore;
+}
 
 const unsubscribe = store.subscribe(() => {
   console.log('Store changed', store.getState());
