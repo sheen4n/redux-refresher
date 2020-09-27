a94b7aea673ec41b1d834e3440d25b69687137bc
Sheen An <sheen.an.goh@gmail.com>
2020-09-27 21:07:59 +0800

    Started AAA

--- redux-refresher-main/src/store/tests/bugs.spec.js
+++ redux-refresher-main/src/store/tests/bugs.spec.js
@@ -1,11 +1,25 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { addBug } from '../bugs';
 import configureStore from '../configureStore';
 
+// Arrange
+// Act
+// Assert
+
 describe('bugsSlice', () => {
-  it('addBug', async () => {
-    const store = configureStore();
+  it('shoud handle the addBug action', async () => {
+    // Arrange
     const bug = { description: 'a' };
+    const savedBug = { ...bug, id: 1 };
+    const fakeAxios = new MockAdapter(axios);
+    fakeAxios.onPost('/bugs').reply(200, savedBug);
+    const store = configureStore();
+
+    // Act
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+
+    // Assert
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
