9464e47b2cbc8e42c40567064b9589472c9d01af
Sheen An <sheen.an.goh@gmail.com>
2020-09-27 20:57:09 +0800

    Bug Fix - Ensure middleware returns

--- redux-refresher-main/src/store/middleware/logger.js
+++ redux-refresher-main/src/store/middleware/logger.js
@@ -5,7 +5,7 @@ const logger = (param) => (store) => (next) => (action) => {
   // console.log('store', store); // Just to see what is in pseudo store
   // console.log('next', next); // See what is next
   console.log('action', action); // See what is the action and payload
-  next(action); // calls next, if is last middleware ,next = reducer
+  return next(action); // calls next, if is last middleware ,next = reducer
 };
 
 export default logger;
