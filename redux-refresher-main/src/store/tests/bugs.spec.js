2e7f99545569674af8555cbf3a92c21910031b52
Sheen An <sheen.an.goh@gmail.com>
2020-09-27 21:11:35 +0800

    Added Error Tests

--- redux-refresher-main/src/store/tests/bugs.spec.js
+++ redux-refresher-main/src/store/tests/bugs.spec.js
@@ -27,4 +27,13 @@ describe('bugsSlice', () => {
 
     expect(bugsSlice().list).toContainEqual(savedBug);
   });
+
+  it('should add no bug to the store if it is saved to the server', async () => {
+    const bug = { description: 'a' };
+    fakeAxios.onPost('/bugs').reply(500);
+
+    await store.dispatch(addBug(bug));
+
+    expect(bugsSlice().list).toHaveLength(0);
+  });
 });
